3530b41d5d72d0cd177289bbf982912d9623ae4d
Peter Csoka <petercsoka@gmail.com>
2021-12-14 22:39:11 +0100

    remove users api

--- pages/api/user.js
+++
@@ -1,8 +0,0 @@
-import { nonce } from "../../db/user"
-
-// Return nonce for a given address
-export default function handler(req, res) {
-    return res
-          .status(200)
-          .send({ nonce });
-}
