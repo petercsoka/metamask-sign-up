bb1f976512007e09e84dd8709cbb7f7bb0bac933
Peter Csoka <petercsoka@gmail.com>
2021-12-14 22:40:02 +0100

    Update and rename auth.js to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
 import Web3 from 'web3'
 import { useEffect, useState } from "react"
 
-async function getUserNonce(addr) {
-    const user_response = await fetch("/api/user?addr="+addr)
-    const user_data = await user_response.json()
-    return user_data.nonce || undefined
-}
-
 export default function Auth() {
     // States
     const [web3, setWeb3] = useState(undefined)
@@ -22,10 +16,8 @@ export default function Auth() {
     const onWeb3Login = async () => {
         // Request account access
         const address = (await web3.eth.requestAccounts())[0]
-        // Fetch user nonce
-        const nonce = await getUserNonce(address)
         // Create message to sign
-        const message = "Hello "+nonce
+        const message = "Hello Prezi!"
         // Request user to sign the signature
         const signature = await web3.eth.personal.sign(message, address)
         // Verify signature and address
